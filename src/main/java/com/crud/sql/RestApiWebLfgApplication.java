0e4fec72937bcbec31228a7c36cb5dc57e616b21
josepmartorell <martorelljosep>
2022-08-23 09:40:19 +0200

    start project

---
+++ src/main/java/com/crud/sql/RestApiWebLfgApplication.java
@@ -0,0 +1,13 @@
+package com.crud.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWebLfgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiWebLfgApplication.class, args);
+	}
+
+}
